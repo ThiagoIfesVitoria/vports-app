02eb42fe29cc09c37c64bd361ea9b578db3bf34b
Thiago Roberto <thiagosroberto.2013@gmail.com>
2025-05-20 11:49:53 +0000

    Atualização de informação

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,16 @@ condicoes = [
 # Defina os valores correspondentes às condições
 valores = ["ENGENHARIA", "PCM/MANUTENÇÃO", "FACILITIES", "PLANEJAMENTO OP.", "OPERAÇÃO", "TI"]
 
+# Definir contato dos supervisores
+contato = {
+    "ENGENHARIA" : ["Ednaldo Lepaus Baldan", "@vports.com.br", "(27) 99895-****"],
+    "PCM/MANUTENÇÃO":["Joao Antonio Oliveira", "jaoliveira@vports.com.br", "(27) 99895-****"],
+    "FACILITIES":["Danylo Nunes Barbosa", "@vports.com.br", "(27) 99895-****"],
+    "PLANEJAMENTO OP.":["Lucas Bozolan Mendes", "@vports.com.br", "(27) 99895-****"],
+    "OPERAÇÃO":["Lucas Bozolan Mendes", "@vports.com.br", "(27) 99895-****"],
+    "TI":["Tiago Boldrini de Azevedo", "@vports.com.br", "(27) 99895-****"]
+}
+
 df["Responsável"] = np.select(condicoes, valores,"NaN")
 
 # Remover colunas matriz de responsabilidade
@@ -31,10 +41,10 @@ for _, row in df.iterrows():
     dicionario_resp[chave] = row["Responsável"]
 
 st.image("assets/Logo.png",width=100)
-st.title("Definição do Responsável em :blue[_ações de segurança_]")
+st.title("Definição de responsabilidade por atividade :blue[(Gestão de ativos)]")
 # Extrair listas únicas
 descricoes = sorted(set(chave[0] for chave in dicionario_resp))
-descricao = st.selectbox("Escolha a DESCRIÇÃO", descricoes)
+descricao = st.selectbox("Escolha o ATIVO/EQUIPAMENTO", descricoes)
 
 # Filtrar as próximas opções com base na anterior
 demandas = sorted(set(k[1] for k in dicionario_resp if k[0] == descricao))
@@ -46,9 +56,25 @@ origem = st.selectbox("Escolha a ORIGEM", origens)
 # Exibir o responsável
 chave = (descricao, demanda, origem)
 responsavel = dicionario_resp.get(chave, "Não encontrado")
-st.write("**Responsável:**", responsavel)
+st.write("**O Responsável pela ação é:**", f":blue[{responsavel}]")
+
+# Define o dialog como uma função decorada
+@st.dialog("Detalhamento do responsável")
+def mostrar_detalhes(resp):
+    st.title(f"Setor Responsável: :blue[{resp}]")
+
+    st.write("## 📞 Contato do Supervisor responsável")
+    st.markdown(f"""
+    - Supervisor responsável: {contato[resp][0]} 
+    - E-mail: {contato[resp][1]}   
+    """)
+
+# Popover com botão que chama o dialog
+
+if st.button(":blue[_mostrar detalhes_]", type="tertiary"):
+    mostrar_detalhes(responsavel)
 
 with st.popover("Relatório de Auditoria"):
-    st.markdown("**Data do Acordo:** 31/07/2003")
+    st.markdown("**Data do Acordo:** 31/07/****")
     st.markdown("**Supervisores Responsáveis:**")
-    st.markdown("- *João Oliveira* (Supervisor do PCM)  \n- *Lucas Teste* (Supervisor de Operações)  \n- *Thiago Nespoli* (Supervisor de T.I)")
+    st.markdown("- *Lucas Bozolan Mendes* (Gerente de Operações) \n- *Joao Antonio Oliveira* (Supervisor do PCM)  \n- *Tiago Boldrini de Azevedo* (Supervisor de T.I)")
